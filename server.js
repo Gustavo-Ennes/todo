9b6709f7db4c37f3927308aace65c5203412c335
Gustavo Ennes <gustavo_ennes@yahoo.com>
2021-02-27 10:26:23 -0300

    first successfull deploy?

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
-const randomId = require('random-id');
 const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
+const apiRouter = require('./routes/apiRouter');
+const indexRouter = require('./routes/indexRouter');
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'my-app', 'public')));
+app.use(express.static(path.join(__dirname, 'my-app', 'dist')));
+app.use('/api/', apiRouter);
+app.use('/', indexRouter);
 
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
